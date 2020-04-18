837d9b1a4d128149184494243d679e2a226de317
Samuel Kinnett <samuelkinnett96@googlemail.com>
2020-04-18 14:03:19 +0100

    Added CollisionType Enum

---
+++ MomenTFS/MomenTFS/MAP/Enums/CollisionType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MomenTFS.MAP.Enums
+{
+    public enum CollisionType
+    {
+        EMPTY = 0,
+        SOLID = 128,
+        EXIT_1 = 110,
+        EXIT_2 = 111,
+        EXIT_3 = 112,
+        EXIT_4 = 113,
+        EXIT_5 = 114,
+        EXIT_6 = 115,
+        EXIT_7 = 116,
+        EXIT_8 = 117,
+        EXIT_9 = 118,
+        EXIT_10 = 119,
+        TOILET = 120
+    }
+}
